bb80be54834534c0379185d455468462fa907f87
Doc Yes <spam@featureblend.com>
2011-07-06 22:03:39 -0700

    Clean up interface.

--- mmm.js
+++ mmm.js
@@ -1,12 +1,12 @@
 var mmm = {};
 (function(ns){
     function Field(){
-        var _value;
+        var value;
         this.get = function() {
-            return _value;
+            return value;
         };
-        this.set = function(value) {
-            _value = value
+        this.set = function(v) {
+            value = v;
         };
     }
     ns.Field = Field;
@@ -16,36 +16,27 @@ var mmm = {};
         this.fields = {
             id: new Field() 
         };
-        for(var field in options.fields || {}){
-            if(this.fields.hasOwnProperty(field)){ 
-                this.fields[field].set(options.fields[field]);
-            }
-        }
+        this.setFields(options.fields || {});
     }
     Model.prototype = {
-        save: function(success, error){
-            var method = (this.fields.id) ? 'update' : 'create';
-            this.datastore(method, success, error); 
-        },
-        parse: function(response){
-            return response;
-        },
-        datastore: function(method, success, error){
-            this.parse(undefined);
-            success(this);
-        },
-        get: function(name){
+        save: function(success, error){},
+        getField: function(name){
             var field = this.fields[name];
             return (field) ? field.get() : undefined;
         },
-        set: function(name, value){
+        setField: function(name, value){
             var field = this.fields[name];
             if(field){
                 field.set(value);
             }
+        },
+        setFields: function(fields){
+            for(var field in fields){
+                this.setField(field, this.fields[field]);
+            }
         }
     };
     ns.Model = Model;
-
+    
 })(mmm);
 
